4531f35d43c147042d08202be1d9dcaa02d5b50a
shan5742 <asamshan456@gmail.com>
2023-04-21 19:50:37 +0100

    add og

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html data-theme="cyberpunk">
-      <Head />
+      <Head>
+        {/* Open Graph tags */}
+        <meta property="og:title" content="Shan8851" />
+        <meta property="og:description" content="Web developer, consultant and mentor" />
+        <meta property="og:image" content="/shan-1200.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.shan8851.com" />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Shan8851" />
+        <meta name="twitter:description" content="Web developer, consultant and mentor" />
+        <meta name="twitter:image" content="/shan-1200/png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
