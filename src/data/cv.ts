c62b3a7ff5f49457150d3cac2c1fbcc8529c1985
shan5742 <asamshan456@gmail.com>
2023-08-24 20:26:13 +0100

    [cv] - cv download

---
+++ src/data/cv.ts
@@ -0,0 +1,49 @@
+export const cvData = {
+  summary: {
+    title: "Summary",
+    content: [
+      "As a software engineer with 5 years of multifaceted experience, I have become proficient in an array of technologies, including React, TypeScript, NextJS, and emerging fields like crypto and Web3. My career has seen me leading complex projects, co-founding innovative startups, and winning top honours at hackathons like Hackmoney, where my team's Mac Insurance project took first place for 'best use' and second place for the 'best app' categories.",
+      "Collaborating closely with over 20 mentees, I've guided aspiring developers into the industry, reflecting my passion for personal and professional growth. Innovative, user-centred, and driven by both challenges and collaboration, I'm committed to pushing the boundaries of technology and nurturing the next generation of tech talent."
+    ]
+  },
+  tech: {
+    title: "🤹🏽 Tech",
+    content: ["My tech stack of choice is React and  Typescript, alongside an array of modern tools and frameworks such as NextJS, Tailwind, GraphQL, TRPC and many more. On the backend side of things I have stayed well within the javascript ecosystem and focussed on Node, Express and a few others. More recently I have picked up some basic Solidity experience and worked with many of the Javascript web3 libraries, most notably EthersJS and Web3JS."]
+  },
+  career: {
+    title: "👔 Career",
+    roles: [{
+      title: "Software Engineer",
+      company: "₿ Cielo Finance",
+      date: "October 2022 - Present",
+      content: [
+        "At Cielo Finance, I've played a key role as a Lead Developer in the creation and growth of a Defi analytics feed that has now become a robust platform with over 10k monthly visitors. Beginning with the first iteration of the Cielo feed, I spearheaded the development, allowing tracking across 16 EVM chains using React and Typescript. Collaborating closely with backend and design teams, I've crafted supplementary dashboards for NFT traders, a comprehensive stats page for wallet trend and on chain activity monitoring, as well as pixel-perfect front-ends that received high praise from both users and stakeholders.",
+        "Recently, I took the lead on Token analytics and Discord integration on the frontend, further enhancing the platform's capabilities. My efforts have been instrumental in driving innovation in the Defi analytics space and launching a successful free tier in April 2023."
+      ]
+    }, {
+      title: "CTO & Co-Founder",
+      company: "🍜 Let's Eat",
+      date: "July 2021 - October 22",
+      content: ["As Co-Founder & CTO of Let's Eat, I embraced a hands-on approach across all stages of the technical product life cycle. From architecture and planning to development, testing, and maintenance, I was instrumental in shaping the technical direction of all 4 Let's Eat applications. My leadership extended to strategic decision-making regarding technology choices, working closely with tech leads to address tech debt, and ensuring the delivery of high-quality solutions that met company SLAs. By proactively identifying and resolving issues related to hardware, software, and network infrastructure, I safeguarded the smooth operations of an organisation that serves thousands of customers annually. My role at Let's Eat epitomises my dedication to innovation, quality, and operational excellence."]
+    }, {
+
+      title: "Software Engineer",
+      company: "📚 Library of Things",
+      date: "Jan 2021 - July 2021",
+      content: ["At Library of Things, I served as a Software Engineer with a focus on creating efficient and scalable solutions. I developed a custom CMS using Typescript and GraphQL, automating content across various locations and channels. This crucial innovation allowed the platform to rapidly scale from 1 to 4 locations within just 6 months, reflecting both agility and foresight. Additionally, I engineered internal tools and protocols for a SaaS that supported multiple admin permissions and levels, laying the groundwork for Library of Things to licence their software in the future.",
+        "Complementing these efforts, I was actively involved in enhancing development operations, supporting the team in creating pipelines for continuous integration and continuous delivery using Gitlab and Docker. My contributions at Library of Things were characterised by a blend of technical skills and a deep understanding of business needs, ensuring that technology always aligned with the company's growth and vision."
+      ]
+    }, {
+      title: "Software Engineer",
+      company: "🖥 NorthLink Digital",
+      date: "Feb 2019 - Jan 2021",
+      content: ["As the inaugural Software Engineer at NorthLink Digital, I played a pivotal role in shaping the company's technical direction and growth. I led a small, agile team of developers to create scalable cloud-first web and mobile applications, overseeing all aspects from reviews and mentoring to sprint planning and retrospectives. One standout project was the delivery of a React Native survey application aimed at promoting sexual and relationship education for students in Asia, now utilised across two continents and reaching thousands of students daily. My innovative work also included building custom boilerplates using NextJS, SaaS, and Sanity CMS, significantly reducing development time for new and junior developers. Regularly attending training courses, conferences, and engaging in code reviews with the CTO, I continuously honed my skills and contributed to the scaling of NorthLink Digital from a single employee to a thriving team of 15."]
+    }, {
+      title: "Mentor",
+      company: "🧑‍🏫️ Coding Coach & The Mentoring Club",
+      date: "Aug 2020 - Present",
+      content: ["As a committed mentor with Coding Coach & The Mentoring Club, I've provided hands-on guidance and training to aspiring developers, empowering them to launch their careers in the software industry. My approach is holistic, covering essential technical skills such as React Hooks, state management, and portfolio development, as well as offering strategic insights into landing that first job and thriving in the early stages of a tech career. Together with my mentees, I've planned tailored learning paths and job-hunting strategies, focusing on aggressive and well-informed approaches. This one-on-one coaching has proven highly effective, with five former mentees now successfully employed in the industry.", "Beyond individual mentoring, I've extended my passion for education by running a newsletter aimed at new developers. Through this platform, I've shared insights, tips, and resources, reaching a broader audience and contributing to the growth of the tech community. My work in mentorship reflects a deep-seated belief in the importance of continuous learning, collaboration, and empowering others to achieve their potential."]
+    }
+    ]
+  }
+}
--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,3 +1,4 @@
+import { cvData } from "@/data/cv";
 import { NextPage } from "next";
 import Head from "next/head";
 
@@ -10,68 +11,33 @@ const Cv: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="p-4 md:p-16 flex flex-col w-screen justify-center max-w-3xl mx-auto">
-          <h1 className="mb-12 text-3xl font-extrabold lg:text-4xl">📋  CV</h1>
-          <h2 className="mb-4 text-2xl font-extrabold lg:text-3xl">🤹🏽 Skills</h2>
-          <p className="mb-6 text-md font-normal lg:text-lg">React, React Native, TypeScript, GraphQL, NodeJS, NextJS, CSS-in-JS, Version Control, ethersjs, Solidity, Hardhat, Express, REST API, AWS (EC2, S3, Route53), Firebase, Docker.</p>
-          <h2 className="mb-4 text-2xl font-extrabold lg:text-3xl">👔 Work</h2>
-          <div className="divider" />
-          <h3 className="mb-4 text-md font-extrabold lg:text-xl">₿ Cielo Finance - <span className="italic text-sm lg:text-lg">Software Engineer</span></h3>
-          <ul className="list-disc">
-            <li>Spearheaded the development of the Cielo - a Defi analytics feed as a Lead Developer at Uniwhales.</li>
-            <li>Took charge of implementing the first iteration of the Cielo feed, which now tracks 16 EVM chains with React and Typescript
-              and has over 25,000 users (as of 03/2023).</li>
-            <li>Worked on supplementary dashboards to help NFT traders identify promising projects and built a comprehensive stats page
-              to monitor wallet trends and activity for various different categories.</li>
-            <li>Collaborated closely with the backend and design teams to ensure seamless integration and a visually stunning user
-              experience.</li>
-            <li>Received high praise from users and stakeholders for my work on the pixel-perfect front-ends and easy-to-use web3
-              integrations.</li>
-            <li>Driving continued innovation in the Defi analytics space as part of this dynamic team. Free tier successfully launched in April 2023</li>
-          </ul>
-          <div className="divider" />
-          <h3 className="mb-4 text-md font-extrabold lg:text-xl">🍜 Let&apos;s Eat - <span className="italic text-sm lg:text-lg">Co-Founder & CTO</span></h3>
-          <ul className="list-disc">
-            <li>Contributed in all stages of the technical product life cycle, including architecture, planning, development, testing,
-              deployment, and maintenance.</li>
-            <li>Directed and consulted on technology choices and technical direction for all 4 Let&apos;s Eat applications.</li>
-            <li>Worked closely with tech leads to address tech debt and ensure timely delivery of high-quality solutions that met company
-              SLAs.</li>
-            <li>Identified and resolved issues related to hardware, software, and network infrastructure that impacted operations of an
-              organization serving thousands of customers annually.</li>
-          </ul>
-          <div className="divider" />
-          <h3 className="mb-4 text-md font-extrabold lg:text-xl">📚 Library of Things - <span className="italic text-sm lg:text-lg">Software Engineer</span></h3>
-          <ul className="list-disc">
-            <li>Built a custom CMS with Typescript and GraphQL that automated content across various locations and channels, enabling
-              the platform to scale from 1 location to 4 in 6 months.</li>
-            <li>Built internal tools and protocols for a SaaS supporting multiple admin permissions and levels in preparation for Library of Things
-              licensing their software.</li>
-            <li>Supported development team in creating pipelines for continuous integration and continuous delivery using Gitlab alongside
-              Docker.
-            </li>
-          </ul>
-          <div className="divider" />
-          <h3 className="mb-4 text-md font-extrabold lg:text-xl">🖥 NorthLink Digital - <span className="italic text-sm lg:text-lg">Software Engineer</span></h3>
-          <ul className="list-disc">
-            <li>Led a small team of developers to create scalable cloud first web and mobile applications. Reviews, mentoring, sprint
-              planning and retros were all part of my responsibilities.</li>
-            <li>Delivered a fully-functional, React Native survey application promoting sexual and relationship education for students in
-              Asia. The app is now rolled out across two continents, with multi-lingual support and used by thousands of students daily.</li>
-            <li>Worked on building out custom boiler plate using NextJS, SaaS and Sanity CMS to quickly spin up new client projects, which
-              dramatically reduced dev time for newer and junior devs.
-            </li>
-            <li>Attended training courses and conferences regularly and underwent frequent code reviews with the CTO to fast-track
-              development skills.</li>
-          </ul>
-           <div className="divider" />
-          <h3 className="mb-4 text-md font-extrabold lg:text-xl">🧑‍🏫️ Coding Coach & The Mentoring Club - <span className="italic text-sm lg:text-lg">Mentor</span></h3>
-          <ul className="list-disc">
-            <li>Provided mentorship and training on topics including React Hooks, state management, and portfolio development to help new developers break into the software industry.</li>
-            <li>Discuss and help plan overall strategy for attacking learning and aggressively hunting for the first job. Once secured, I work with mentees to develop a strategy to accelerate the early years of their career.</li>
-            <li>Five former mentees are now employed in the industry.
-            </li>
-          </ul>
+        <div className="p-4 md:p-16 flex flex-col w-screen justify-center max-w-5xl mx-auto">
+          <div className="flex gap-2 items-center mb-12">
+            <h1 className="text-3xl font-extrabold lg:text-4xl">📋 CV</h1>
+            <a className="badge badge-accent" href="/cv.pdf" download>
+              download
+            </a>
+          </div>
+          <h2 className="mb-4 text-2xl font-extrabold lg:text-3xl">{cvData.summary.title}</h2>
+          {cvData.summary.content.map((item, index) => (
+            <p key={index} className="mb-6 text-md font-normal lg:text-lg">{item}</p>
+          ))}
+          <h2 className="mb-4 text-2xl font-extrabold lg:text-3xl">{cvData.tech.title}</h2>
+          {cvData.tech.content.map((item, index) => (
+            <p key={index} className="mb-6 text-md font-normal lg:text-lg">{item}</p>
+          ))}
+          <h2 className="mb-4 text-2xl font-extrabold lg:text-3xl">{cvData.career.title}</h2>
+          {cvData.career.roles.map((role, roleIndex) => (
+            <div key={roleIndex}> {/* Wrap the contents inside a div */}
+              <h3 className="mb-4 text-md font-extrabold lg:text-xl">{role.company} - <span className="italic text-sm lg:text-lg">{role.title}</span> - {role.date}</h3>
+              {role.content.map((item, index) => (
+                <p key={index} className="mb-6 text-md font-normal lg:text-lg">{item}</p>
+              ))}
+            </div>
+          ))}
+          <a className="btn btn-neutral" href="/cv.pdf" download>
+            Download My CV
+          </a>
         </div>
       </main>
     </>
